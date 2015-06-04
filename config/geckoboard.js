10db7e2048241f8624579f24155a3f5d28eae9db
Ian Williams <me@ianwillia.ms>
2015-06-04 17:42:55 -0400

    POST to geckoboard widget

---
+++ config/geckoboard.js
@@ -0,0 +1,5 @@
+module.exports = {
+	apiUrl: 'https://push.geckoboard.com/v1/send/',
+	widgetKey: '146669-b27e1f9d-65de-401e-9a4e-cabbb86dc9bb',
+	apiKey: '58f92837c641fbd8f895d5ca817ee4a9'	
+};
---
+++ index.js
@@ -0,0 +1,29 @@
+var request = require('superagent');
+var geckoboard = require('./config/geckoboard');
+
+
+var postToWidget = function(data) {
+	return request.post(geckoboard.apiUrl + geckoboard.widgetKey)
+	.send({
+		api_key: geckoboard.apiKey,
+		data: data
+	})
+}
+
+var textData = {
+	item: []
+}
+
+process.argv.forEach(function (val, index, array) {
+	if (index < 2) return;
+	textData.item.push({
+		text: val
+	})
+});
+
+postToWidget(textData)
+.end(function(err, res){
+	if (err) return console.log(err);
+	console.log('Successfully updated the widget.');
+});
+
